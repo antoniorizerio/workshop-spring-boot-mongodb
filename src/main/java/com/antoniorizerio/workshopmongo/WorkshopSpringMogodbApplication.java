9ff15bd1417a12d1d9fcddc2c2fff68bf13f1560
Antonio Rizerio <antonio.rizerio@gmail.com>
2023-06-02 20:11:15 -0300

    Criando projeto Spring com Mongodb

---
+++ src/main/java/com/antoniorizerio/workshopmongo/WorkshopSpringMogodbApplication.java
@@ -0,0 +1,13 @@
+package com.antoniorizerio.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMogodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMogodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniorizerio/workshopmongo/WorkshopSpringMogodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniorizerio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMogodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
